39645c815b260d1b01bc4fd5dae04e2c93c11df4
Brandon Hall <brandon.hall@kimley-horn.com>
2024-05-29 13:34:36 -0400

    Cleaning up RampCollector code

--- RampMeterCollector/Program.cs
+++ RampMeterCollector/Program.cs
@@ -1,5 +1,3 @@
-using System.Threading.Tasks;
-
 namespace RampMeterCollector
 {
     class Program
--- RampMeterCollector/RampCollector.cs
+++ RampMeterCollector/RampCollector.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using System.Xml.Linq;
-using Apache.Arrow;
 using Parquet;
 using Parquet.Schema;
 using System.Configuration;
-using System.Globalization;
 using MySqlConnector;
 
 namespace RampMeterCollector
@@ -18,8 +15,9 @@ namespace RampMeterCollector
         internal static readonly string MySqlDbName = ConfigurationManager.AppSettings["DB_NAME"] ?? "mark1";
         internal static readonly string? MySqlConnString = ConfigurationManager.AppSettings["CONN_STRING"];
         internal static MySqlConnection MySqlConnection;
-        internal string RampString = String.Empty;
+        internal string RampString = string.Empty;
         internal List<RampEvent> RampEvents = [];
+        internal string HistoryDir = ConfigurationManager.AppSettings["HISTORY_DIR"] ?? "C:\\";
 
         #region Startup
 
@@ -35,8 +33,6 @@ namespace RampMeterCollector
                 var xmlData = await CollectEvents();
                 var xmlDoc = XDocument.Parse(xmlData);
                 ConvertXmlToEvent(xmlDoc);
-                Console.WriteLine("Root Element:");
-                Console.WriteLine(xmlDoc.Root);
                 await WriteEventsToParquet(RampEvents, "Ramp Meter History");
             }
             catch (Exception ex)
@@ -52,9 +48,6 @@ namespace RampMeterCollector
         {
             var formattedTime = GetFormattedTimeOneMinuteAgo();
             var requestUrl = Url + formattedTime;
-
-            Console.WriteLine($"Request URL: {requestUrl}");
-
             using var client = new HttpClient();
             var response = await client.GetAsync(requestUrl);
 
@@ -64,113 +57,91 @@ namespace RampMeterCollector
                 RampString += xmlData;
                 return xmlData;
             }
-            else
-            {
-                var errorMessage = $"Failed to fetch XML data. Status code: {response.StatusCode}";
-                if (response.Content == null) throw new HttpRequestException(errorMessage);
-                var responseBody = await response.Content.ReadAsStringAsync();
-                errorMessage += $"\nResponse body: {responseBody}";
-                throw new HttpRequestException(errorMessage);
-            }
+
+            var errorMessage = $"Failed to fetch XML data. Status code: {response.StatusCode}";
+            if (response.Content == null) throw new HttpRequestException(errorMessage);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            errorMessage += $"\nResponse body: {responseBody}";
+            throw new HttpRequestException(errorMessage);
+
         }
 
         #region Helper Methods
 
         private static string GetFormattedTimeOneMinuteAgo()
         {
-            // Original DateTime
             var originalDateTime = DateTime.Now;
-            // Convert to DateTimeOffset
             var localDateTimeOffset = new DateTimeOffset(originalDateTime);
-            // Define the target time zone (e.g., Eastern Standard Time)
             var targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-
-            // Convert to the target time zone
-            var targetDateTimeOffset = TimeZoneInfo.ConvertTime(localDateTimeOffset, targetTimeZone);
-            Console.WriteLine("Target DateTimeOffset (Eastern Standard Time): " + targetDateTimeOffset);
-            // Get the current time and subtract one minute
+            TimeZoneInfo.ConvertTime(localDateTimeOffset, targetTimeZone);
             var minusOne = originalDateTime.AddMinutes(-1);
-            // Format the datetime to the specified format
             var formattedTime = minusOne.ToString("MM-dd-yyyy H:mm:ss.f");
 
             return formattedTime;
         }
 
-        private Task ConvertDataTableToParquet()
+        private Task ConvertParquetToCSV()
         {
 
             return null;
         }
 
-    public void ConvertXmlToEvent(XDocument doc)
-    {
-        if (string.IsNullOrEmpty(RampString))
+        public void ConvertXmlToEvent(XDocument doc)
         {
-            Console.WriteLine("No Ramp Events");
-            return;
+            if (string.IsNullOrEmpty(RampString))
+            {
+                Console.WriteLine("No Ramp Events");
+                return;
+            }
+
+            var events = doc.Descendants("EventResponses")
+                            .Descendants("Event")
+                            .Select(e => new RampEvent
+                            {
+                                Id = (int)e.Attribute("ID"),
+                                TimeStamp = DateTime.ParseExact((string)e.Attribute("TimeStamp"), "MM-dd-yyyy HH:mm:ss.f", null),
+                                EventTypeId = (int)e.Attribute("EventTypeID"),
+                                Parameter = (int)e.Attribute("Parameter")
+                            })
+                            .ToList();
+
+            RampEvents.AddRange(events);
+#if DEBUG
+            foreach (var rampEvent in events)
+            {
+                Console.WriteLine($"ID: {rampEvent.Id}, TimeStamp: {rampEvent.TimeStamp}, Event Type: {rampEvent.EventTypeId}, Parameter: {rampEvent.Parameter}");
+            }
+#endif
         }
-
-        var tester = doc.FirstNode.Document.FirstNode;
-        Console.WriteLine(tester);
-        
-        var events = doc.Descendants("EventResponses")
-                        .Descendants("Event")
-                        .Select(e => new RampEvent
-                        {
-                            Id = (int)e.Attribute("ID"),
-                            TimeStamp = DateTime.ParseExact((string)e.Attribute("TimeStamp"), "MM-dd-yyyy HH:mm:ss.f", null),
-                            EventTypeId = (int)e.Attribute("EventTypeID"),
-                            Parameter = (int)e.Attribute("Parameter")
-                        })
-                        .ToList();
-
-        RampEvents.AddRange(events);
-
-        foreach (var rampEvent in events)
-        {
-            Console.WriteLine($"ID: {rampEvent.Id}, TimeStamp: {rampEvent.TimeStamp}, Event Type: {rampEvent.EventTypeId}, Parameter: {rampEvent.Parameter}");
-        }
-    }
         #endregion
 
         #region Convert To Parquet
 
         private async Task WriteEventsToParquet(List<RampEvent> events, string folderName)
         {
-        // Determine the project directory
-        string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.Parent.Parent.FullName;
+            var fullPath = Path.Combine(HistoryDir, folderName);
 
-        // Combine the project directory with the History folder name
-        string fullPath = Path.Combine(projectDirectory, folderName);
-
-        if (!Directory.Exists(fullPath))
-        {
-            Directory.CreateDirectory(fullPath);
-        }
-
-            string filePath = Path.Combine(fullPath, $"ramp_meter_events_{DateTime.Now:yyyy-MM-dd_HHmmss}.parquet");
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+            }
+            var filePath = Path.Combine(fullPath, $"ramp_meter_events_{DateTime.Now:yyyy-MM-dd_HHmmss}.parquet");
 
             var fields = new List<DataField>
-        {
-            new DataField<int>("Id"),
-            new DateTimeDataField("TimeStamp", DateTimeFormat.DateAndTime), // Correct way to handle DateTime
-            new DataField<int>("EventTypeId"),
-            new DataField<int>("Parameter")
-        };
-
-            using (Stream fileStream = File.Create(filePath))
-            {
-                using (var parquetWriter = await ParquetWriter.CreateAsync(new ParquetSchema(fields.ToArray()), fileStream))
                 {
-                    using (ParquetRowGroupWriter rowGroupWriter = parquetWriter.CreateRowGroup())
-                    {
-                        await WriteColumnAsync(rowGroupWriter, fields[0], events.Select(e => e.Id).ToArray());
-                        await WriteColumnAsync(rowGroupWriter, fields[1], events.Select(e => e.TimeStamp).ToArray());
-                        await WriteColumnAsync(rowGroupWriter, fields[2], events.Select(e => e.EventTypeId).ToArray());
-                        await WriteColumnAsync(rowGroupWriter, fields[3], events.Select(e => e.Parameter).ToArray());
-                    }
-                }
-            }
+                    new DataField<int>("Id"),
+                    new DateTimeDataField("TimeStamp", DateTimeFormat.DateAndTime),
+                    new DataField<int>("EventTypeId"),
+                    new DataField<int>("Parameter")
+                };
+
+            await using Stream fileStream = File.Create(filePath);
+            using var parquetWriter = await ParquetWriter.CreateAsync(new ParquetSchema(fields.ToArray()), fileStream);
+            using var rowGroupWriter = parquetWriter.CreateRowGroup();
+            await WriteColumnAsync(rowGroupWriter, fields[0], events.Select(e => e.Id).ToArray());
+            await WriteColumnAsync(rowGroupWriter, fields[1], events.Select(e => e.TimeStamp).ToArray());
+            await WriteColumnAsync(rowGroupWriter, fields[2], events.Select(e => e.EventTypeId).ToArray());
+            await WriteColumnAsync(rowGroupWriter, fields[3], events.Select(e => e.Parameter).ToArray());
         }
 
         private static async Task WriteColumnAsync<T>(ParquetRowGroupWriter rowGroupWriter, DataField field, T[] data)
@@ -178,20 +149,20 @@ namespace RampMeterCollector
             var dataColumn = new Parquet.Data.DataColumn(field, data);
             await rowGroupWriter.WriteColumnAsync(dataColumn);
         }
-    
-    #endregion
 
-    #region Error Logging
+        #endregion
 
-    /// <summary>
-    /// The overloaded method that will write to the error log
-    /// </summary>
-    /// <param name="applicationName">The name of the file the error is coming from</param>
-    /// <param name="functionName">The name of the function the error is coming from</param>
-    /// <param name="ex">The exception being thrown</param>
-    /// <returns>A task since the method is asynchronous</returns>
-    public static async Task WriteToErrorLog(string applicationName,
-     string functionName, Exception ex)
+        #region Error Logging
+
+        /// <summary>
+        /// The overloaded method that will write to the error log
+        /// </summary>
+        /// <param name="applicationName">The name of the file the error is coming from</param>
+        /// <param name="functionName">The name of the function the error is coming from</param>
+        /// <param name="ex">The exception being thrown</param>
+        /// <returns>A task since the method is asynchronous</returns>
+        public static async Task WriteToErrorLog(string applicationName,
+         string functionName, Exception ex)
         {
             await WriteToErrorLog(applicationName, functionName, ex.Message,
                 ex.InnerException?.ToString());
